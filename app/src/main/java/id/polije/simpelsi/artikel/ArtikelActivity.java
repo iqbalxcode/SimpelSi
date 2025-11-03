49f057b4f98f2459ca742425c7c810137669dc09
farida-hub <faridanovaliamasda24995@gmail.com>
2025-11-03 15:56:03 +0700

    ok

--- app/src/main/java/id/polije/simpelsi/artikel/ArtikelActivity.java
+++ app/src/main/java/id/polije/simpelsi/artikel/ArtikelActivity.java
@@ -1,5 +1,14 @@
 package id.polije.simpelsi.artikel;
 
-public class ArtikelActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import id.polije.simpelsi.R;
 
+public class ArtikelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artikel);
+    }
 }
